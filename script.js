4d95c169d21504484b5229870fcdfd1552846564
Mac <oluwasolabayo@gmail.com>
2026-06-20 23:36:46 +0100

    Add Tokyo back

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const clockConfigs = [
     { name: 'Abeokuta', timezone: 'Africa/Lagos' },
     { name: 'New York', timezone: 'America/New_York' },
     { name: 'London', timezone: 'Europe/London' },
+    { name: 'Tokyo', timezone: 'Asia/Tokyo' },
     { name: 'India', timezone: 'Asia/Kolkata' },
     { name: 'Malawi', timezone: 'Africa/Blantyre' }
 ];
